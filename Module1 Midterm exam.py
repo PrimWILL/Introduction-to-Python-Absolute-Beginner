8781c584ed6817071a23a97239c5680d497cbe9e
PrimWILL <51026374+PrimWILL@users.noreply.github.com>
2019-09-22 09:22:30 +0900

    Module1. end of Mod coding assignment
    simple test+-+

---
+++ Module1 Midterm exam.py
@@ -0,0 +1,13 @@
+# Create Allergy check code
+# then PASTE THIS CODE into edX
+
+# [ ] get input for input_test variable
+food = input("enter somethings eaten in last 24 hrs: ")
+# [ ] print "True" message if "dairy" is in the input or False message if not
+print('It is', 'dairy'.lower() in food.lower(), 'that "'+ food +'" contains "dairy"')
+# [ ] print True message if "nuts" is in the input or False if not
+print('It is', 'nuts'.lower() in food.lower(), 'that "'+ food +'" contains "nuts"')
+# [ ] Challenge: Check if "seafood" is in the input - print message
+print('It is', 'seafood'.lower() in food.lower(), 'that "'+ food +'" contains "seafood"')
+# [ ] Challenge: Check if "chocolate" is in the input - print message
+print('It is', 'chocolate'.lower() in food.lower(), 'that "'+ food +'" contains "chocolate"')
